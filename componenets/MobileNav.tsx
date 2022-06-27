97e0a72e7a6dfeefa1f1472a8bfb2f5a994a0fc8
Richard <haggio0844@gmail.com>
2022-06-27 21:45:10 +0200

    implement mobile navigation 📱

---
+++ componenets/MobileNav.tsx
@@ -0,0 +1,34 @@
+import classes from './MobileNav.module.css';
+import { useState } from 'react';
+import Link from 'next/link';
+import SvgComponent from './SvgComponent';
+
+const MobileNav = () => {
+	const [isActive, setIsActive] = useState(false);
+
+	const toggleActive = () => {
+		setIsActive(!isActive);
+	};
+
+	return (
+		<header className={classes.mobile}>
+			<button
+				onClick={toggleActive}
+				className={`${classes.hamburger} ${isActive ? classes.active : ''}`}>
+				<div className={classes.bar}></div>
+			</button>
+			<nav className={`${classes.mobile_nav} ${isActive ? classes.active : ''}`}>
+				<Link href='/'>Premium</Link>
+				<Link href='/'>Support</Link>
+				<Link href='/'>Download</Link>
+				<hr className={classes.line}></hr>
+				<Link href='/'>Sign up</Link>
+				<Link href='/'>Log in</Link>
+				<SvgComponent className={classes.svg} />
+			</nav>
+			
+		</header>
+	);
+};
+
+export default MobileNav;
--- componenets/Nav.tsx
+++ componenets/Nav.tsx
@@ -1,21 +1,23 @@
 import classes from './Nav.module.css';
 import Link from 'next/link';
 import SvgComponent from './SvgComponent';
+import MobileNav from './MobileNav';
 
 const Nav: React.FC = () => {
-    
-
-    return <nav className={classes.nav}>
-        <SvgComponent className={classes.svg} />
-            <ul className={classes.nav_links}>
-                <Link href='/'>Premium</Link>
-                <Link href='/'>Support</Link>
-                <Link href='/'>Download</Link>
-                <span className={classes.line}></span>
-                <Link href='/'>Sign Up</Link>
-                <Link href='/'>Log In</Link>
-            </ul>
-    </nav>
-}
+	return (
+		<nav className={classes.nav}>
+			<SvgComponent className={classes.svg} />
+			<ul className={classes.nav_links}>
+				<Link href='/'>Premium</Link>
+				<Link href='/'>Support</Link>
+				<Link href='/'>Download</Link>
+				<span className={classes.line}></span>
+				<Link href='/'>Sign up</Link>
+				<Link href='/'>Log in</Link>
+			</ul>
+			<MobileNav />
+		</nav>
+	);
+};
 
 export default Nav;
