b8dbd3c350dcafa8aa5242fe9c754e14bff291af
Richard <haggio0844@gmail.com>
2022-06-29 15:06:22 +0200

    Reconfigured Button 🔘

--- componenets/Section.tsx
+++ componenets/Section.tsx
@@ -1,5 +1,4 @@
 import Button from './Ui/Button';
-import { ButtonT } from './Ui/Button';
 import Link from 'next/link';
 import classes from './Section.module.css';
 
@@ -12,8 +11,8 @@ const Section = () => {
                     <br></br>
 					<h2>Just R59.99/month after. Cancel anytime.</h2>
 					<div id={classes.buttons}>
-                        <Button text='Get Started' />
-                        <ButtonT text='View Plans' />
+						<Button text='Get Started' color={true} />
+                        <Button text='View Plans' color={false} />
 					</div>
 					<p><Link href='/'>Terms and conditions apply</Link>. 1 month free not available for users who have already tried Premium.</p>
 				</div>
--- componenets/Ui/Button.tsx
+++ componenets/Ui/Button.tsx
@@ -2,14 +2,17 @@ import classes from './Button.module.css';
 
 interface Prop {
 	text: string;
+	color: boolean;
 }
 
-const Button: React.FC<Prop> = (props) => {
-	return <button className={`${classes.button}`}>{props.text}</button>;
-};
-
-export const ButtonT: React.FC<Prop> = (props) => {
-	return <button className={classes.buttonT}>{props.text}</button>;
+const Button: React.FC<Prop> = ({ text, color }) => {
+	return (
+		<button
+			className={`${classes.button} ${color ? classes.black : classes.blue}`}
+		>
+			{text}
+		</button>
+	);
 };
 
 export default Button;
