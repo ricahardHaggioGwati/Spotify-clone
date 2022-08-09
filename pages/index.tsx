32bf1db91679bbafe8a1396328261c5da2623286
Richard <haggio0844@gmail.com>
2022-08-09 19:36:09 +0200

    Spotify web 🖥️

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next';
 import Nav from '../components/Ui/Nav';
 import Footer from '../components/Ui/Footer';
-import Section from '../components/index_page_components/Sections/Section';
-import PlanSection from '../components/index_page_components/Sections/PlanSection';
-import PremiumSection from '../components/index_page_components/Sections/PremiumSection';
+import Section from '../components/Home/Sections/Section';
+import PlanSection from '../components/Home/Sections/PlanSection';
+import PremiumSection from '../components/Home/Sections/PremiumSection';
 
 const Home: NextPage = () => {
 	return (
