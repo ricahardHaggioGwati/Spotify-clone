c6e1b99da0813429f757d5f689d9569fd16f4931
Richard Gwati <haggio0844@gmail.com>
2022-09-15 20:32:57 +0200

    Added meta data ♟️

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Spotify</title>
+				<link rel='icon' type='image/x-icon' href='/King.svg' />
+				<meta name='description' content='Spotify clone' />
+				<meta name='keywords' content='spotify web clone' />
+				<meta name='author' content='R.G' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
